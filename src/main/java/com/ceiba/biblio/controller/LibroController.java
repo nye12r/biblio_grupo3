b542555db1292036060e198b30b38df4ca6e6d20
ANGIE VANESSA JARAMILLO CASTRILLON <angie.jaramillo@audi.d1>
2020-11-28 13:58:03 -0500

    creacion getinfo

--- src/main/java/com/ceiba/biblio/controller/LibroController.java
+++ src/main/java/com/ceiba/biblio/controller/LibroController.java
@@ -1,4 +1,20 @@
 package com.ceiba.biblio.controller;
 
+import com.ceiba.biblio.service.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LibroController {
+    
+    @Autowired
+    private LibroService libroService;
+    
+    @GetMapping("/getInfo")
+    public ResponseEntity getInfo(){
+        return libroService.getInfo();
+    }
+    
 }
