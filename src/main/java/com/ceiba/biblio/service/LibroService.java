410e0745ddd87c2a0bc566241c8473f9eb2c2b52
ANGIE VANESSA JARAMILLO CASTRILLON <angie.jaramillo@audi.d1>
2020-11-28 13:50:37 -0500

    creacio servicio libro

--- src/main/java/com/ceiba/biblio/service/LibroService.java
+++ src/main/java/com/ceiba/biblio/service/LibroService.java
@@ -1,4 +1,19 @@
 package com.ceiba.biblio.service;
 
+import com.ceiba.biblio.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LibroService {
+    
+    @Autowired
+    private LibroRepository libroRepository;
+    
+    public ResponseEntity getInfo(){
+        
+        return "ServiciLibro";
+    }
+    
 }
